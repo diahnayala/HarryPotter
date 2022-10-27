6ba38651a1260aacea0c4232138e3f0267b68110
Diana Ayala <diana.ayala@improving.com>
2022-10-27 09:06:12 -0500

    info's page responsive

--- src/Pages/Player.jsx
+++ src/Pages/Player.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import '../css/player.css';
-import dataMovies from '../json/movies.json';
+import "../css/player.css";
+import dataMovies from "../json/movies.json";
 
 function Player() {
   return (
@@ -8,21 +8,21 @@ function Player() {
       <div className="containerIframe">
         <iframe
           className="iframeStyle"
-          width="640"
-          height="360"
-          frameborder="0"
           src="https://mega.nz/embed/KIpATSrZ#tj2KEHtWHBu8FP_d78jWPQqvq24JQqD7DJCfdIwBknY"
+          width="1920"
+          height="1080"
+          frameborder="0"
           allowfullscreen
         ></iframe>
       </div>
-      <div className="iframeComments">
-
-      </div>
+      <div className="iframeComments"></div>
       <div className="info">
-        {dataMovies.map(()=>{
-          return <div>
-            <h1>{dataMovies.name}</h1>
-          </div>
+        {dataMovies.map(() => {
+          return (
+            <div>
+              <h1>{dataMovies.name}</h1>
+            </div>
+          );
         })}
       </div>
     </div>
